f0497868d5ecaddfaa5385554fde685cb23eb162
gaspym97 <angelgasparmiralles@gmail.com>
2025-10-06 16:15:58 -0400

    Initial commit - basic To-Do App structure

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("task-input")
+const addBtn = document.getElementById("add-task-btn")
+const taskList = document.getElementById("task-list")
+
+addBtn.addEventListener('click', () => {
+    const taskText = input.value // get input value
+})
