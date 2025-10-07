7ff1a89d262eaa1a4ce7abbd34d443e359809b28
gaspym97 <angelgasparmiralles@gmail.com>
2025-10-07 17:58:22 -0400

    mark tasks as completed or pending functionality

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ const input = document.getElementById("task-input")
 const addBtn = document.getElementById("add-task-btn")
 const taskList = document.getElementById("task-list")
 
-// When the button is pressed, create new li
+// create new tasks
 addBtn.addEventListener('click', () => {
 
     // Store the text in a variable
     const text = input.value.trim() //trim() to sanitize user input
-
     // create a new li
     if (text === "") return // prevent empty tasks
-    const newTask = document.createElement('li')
 
+    const newTask = document.createElement('li')
     // store the text inside the new li
     newTask.textContent = text
+    // always add the "task" class
+    newTask.classList.add('task')
 
     // append it to the existing ul
     taskList.appendChild(newTask)
+})
 
+// change taks to completed
+taskList.addEventListener('click', (e) => {
+    const task = e.target
+
+    // mark as completed or pending
+    if (task.className === 'taskPending') {
+        task.className = 'taskCompleted'
+    }
+    else {
+        task.className = 'taskPending'
+    }
 })
