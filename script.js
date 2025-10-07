6a8242b5fb6e03bbfadf89315706e180c08e2f21
gaspym97 <angelgasparmiralles@gmail.com>
2025-10-07 12:01:38 -0400

    create new task and appends to list

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const input = document.getElementById("task-input")
 const addBtn = document.getElementById("add-task-btn")
 const taskList = document.getElementById("task-list")
 
+// When the button is pressed, create new li
 addBtn.addEventListener('click', () => {
-    //
+
+    // Store the text in a variable
+    const text = input.value.trim() //trim() to sanitize user input
+
+    // create a new li
+    if (text === "") return // prevent empty tasks
+    const newTask = document.createElement('li')
+
+    // store the text inside the new li
+    newTask.textContent = text
+
+    // append it to the existing ul
+    taskList.appendChild(newTask)
+
 })
